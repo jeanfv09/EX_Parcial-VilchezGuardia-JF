4a8110cf1918611386303e14d27539469a500600
jeanfv09 <jeanfrancov09@gmail.com>
2025-10-03 23:06:53 -0500

    Init: proyecto MVC para examen parcial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EX_Parcial_VilchezGuardia_JF.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
